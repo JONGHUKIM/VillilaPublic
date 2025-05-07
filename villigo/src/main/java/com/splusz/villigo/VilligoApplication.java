b8912af3ed2e95edeb4cac0895beff4fc4955d3a
JONGHUKIM <hotsky803@gmail.com>
2025-05-07 17:40:12 +0900

    aws rds 키 비공개

--- villigo/src/main/java/com/splusz/villigo/VilligoApplication.java
+++ villigo/src/main/java/com/splusz/villigo/VilligoApplication.java
@@ -1,5 +1,6 @@
 package com.splusz.villigo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,8 +9,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class VilligoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VilligoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // .env 파일 로딩
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing() // .env 없을 경우 무시
+                .load();
 
+        // 환경변수 → 시스템 프로퍼티로 설정 (Spring Boot가 인식하게)
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(VilligoApplication.class, args);
+    }
 }
