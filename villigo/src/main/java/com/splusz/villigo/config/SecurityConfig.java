ec6f1cc6c2763849e3be7e254a002009a2a19b3f
JONGHUKIM <hotsky803@gmail.com>
2025-05-16 23:36:41 +0900

    http -> https 강제 변환

--- villigo/src/main/java/com/splusz/villigo/config/SecurityConfig.java
+++ villigo/src/main/java/com/splusz/villigo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.splusz.villigo.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,6 +28,10 @@ import lombok.extern.slf4j.Slf4j;
 @EnableMethodSecurity
 //-> 각각의 컨트롤러 메서드에서 인증(로그인), 권한 설정을 하기 위해서 
 public class SecurityConfig {
+	
+	@Autowired
+	private HttpsEnforcedRedirectStrategy redirectStrategy;
+
 	
 	// Spring Security 5 버전부터 비밀번호는 반드시 암호화(encoding)를 해야만 함 
 	// 만약 비밀번호를 암호화하지 않으면 HTTP 403(access denied, 접근 거부) 또는
@@ -74,7 +79,15 @@ public class SecurityConfig {
 				.loginPage("/member/signin")
 				.loginProcessingUrl("/member/signin")
 				.defaultSuccessUrl("/home", true)
+				.permitAll()
 		);
+		
+	    http.exceptionHandling(exception -> 
+        exception.authenticationEntryPoint((request, response, authException) -> {
+            // redirectStrategy 사용
+            redirectStrategy.sendRedirect(request, response, "/member/signin");
+        })
+    );
 		
 		// 구글 로그인 인증 설정
 		http
