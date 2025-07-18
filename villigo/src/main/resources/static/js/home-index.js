15a731144eb41a1c7a3429ec1f20b63afad3a7e8
JONGHUKIM <hotsky803@gmail.com>
2025-07-18 19:13:31 +0900

    home-index.js 로컬 경로 수정

--- villigo/src/main/resources/static/js/home-index.js
+++ villigo/src/main/resources/static/js/home-index.js
@@ -20,12 +20,12 @@ const brandData = {
     bag: bagbrand.map(brand => ({
         id: brand.id,
         name: brand.name,
-        img: `/images/test/${brand.imagePath}`,
+        img: `/images/test/` + brand.imagePath,
     })),
     car: carbrand.map(brand => ({
         id: brand.id,
         name: brand.name,
-        img: `/images/test/${brand.imagePath}`,
+        img: `/images/test/` + brand.imagePath,
     }))
 };
 
